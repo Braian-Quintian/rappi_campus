6d95cc534788e3234d5593aa22d3cb681dc4c5b3
Braian-Quintian <bquintianromero@gmail.com>
2023-08-24 13:39:50 -0500

    feat: :sparkles: Se añaden los limit

---
+++ src/config/limit.js
@@ -0,0 +1,26 @@
+import rateLimit from "express-rate-limit";
+
+export let limitLogin = ()=>{
+    return rateLimit({
+        windowMs: 1 * 60 * 1000,
+        max: 3,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        skip: (req,res)=>{if(req.headers["content-length"]>30){res.status(413).send({message: "Tamaño de la solicitud alcanzado"});
+                return true;
+            }
+        }, 
+        message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
+        }
+    })    
+}
+export let limitRepartidores = ()=>{
+    return rateLimit({
+        windowMs: 30 * 1000,
+        max: 5,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
+        }
+    })
+}
