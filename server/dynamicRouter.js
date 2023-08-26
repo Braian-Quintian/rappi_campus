68e1c5c5b0c762f305893043e706d22a83c49213
dVera17 <dsvera174@gmail.com>
2023-08-26 12:49:44 -0500

    feat: :sparkles: Se añade la ruta clientes
    Se añade la ruta clientes

--- server/dynamicRouter.js
+++ server/dynamicRouter.js
@@ -1,9 +1,12 @@
 import { router as Repartidores } from '../src/middleware/repartidores.js';
+import { router as Clientes } from '../src/routes/clientes.routes.js';
 export async function dynamicRouter(req, res, next) {
     const { collection } = req.params;
     switch (collection) {
         case 'repartidores':
             return Repartidores(req, res, next);
+        case 'clientes':
+            return Clientes(req, res, next);
         default:
             return res.status(404).send({ error: 'Ruta no encontrada' });
     }
