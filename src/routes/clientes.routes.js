c04c93a5777b780c3d7594a0bb3717419fedd52c
dVera17 <dsvera174@gmail.com>
2023-08-28 12:16:22 -0500

    feat: :sparkles: Ahora los clientes pueden modificar sus datos
    Ahora los clientes pueden modificar sus datos

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import routesVersioning from 'express-routes-versioning';
 import { limitRepartidores } from "../config/limit.js";
-import { deleteCliente, getClientes, postCliente } from "../versiones/v2/clientes.js";
+import { deleteCliente, getClientes, postCliente, putCliente } from "../versiones/v2/clientes.js";
 import { validateDTOcliente } from "../DTO/clientes.js";
 const router = Router();
 const version = routesVersioning();
@@ -13,6 +13,10 @@ router.get('/', version({
 
 router.post('/', validateDTOcliente, version({
     "2.0.0": postCliente
+}))
+
+router.put('/:dni', version({
+    "2.0.0": putCliente
 }))
 
 router.delete('/:dni', version({
--- src/versiones/v2/clientes.js
+++ src/versiones/v2/clientes.js
@@ -34,6 +34,24 @@ export const postCliente = async (req, res) => {
     }
 }
 
+export const putCliente = async (req, res) => {
+    try {
+        const clienteData = {
+            cli_primerNombre: req.body.cli_primerNombre,
+            cli_primerApellido: req.body.cli_primerApellido,
+            cli_telefono: req.body.cli_telefono,
+            cli_email: req.body.cli_email,
+            cli_fecha_nac: new Date(req.body.cli_fecha_nac),
+            cli_genero: req.body.cli_genero,
+        };
+        let result = await clientes.updateOne({ cli_dni: parseInt(req.params.dni) }, { "$set": clienteData });
+        console.log(result);
+        res.send(result);
+    } catch (error) {
+        res.send(error);
+    }
+}
+
 export const deleteCliente = async (req, res) => {
     try {
         let result = await clientes.deleteOne({ "cli_dni": parseInt(req.params.dni) })
