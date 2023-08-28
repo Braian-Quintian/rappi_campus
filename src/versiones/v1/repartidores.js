3866de1eed4eb92509f31058cb05c2ec98ecb32f
Braian-Quintian <bquintianromero@gmail.com>
2023-08-28 12:34:32 -0500

    feat: :sparkles: Se añade el código para traer la informacion de todos los repartidores

--- src/versiones/v1/repartidores.js
+++ src/versiones/v1/repartidores.js
@@ -1,5 +1,26 @@
-export const repartidoresV1 = (req,res,next) => {
+import { connect } from '../../connection/connection.js'
+const db = await connect();
+
+export const repartidoresV1 = async (req,res,next) => {
     if(!req.rateLimit) return; 
-    console.log("ESTAMOS EN REPARTIDORES");
-    console.log("se redirecciona a router llamando los metodos")
+    try {
+        const result =  await db.collection('repartidores').aggregate([
+            {
+                $project: {
+                    _id: 0,
+                    "dni-repartidor": "$rep_dni",
+                    "nombre-repartidor": "$rep_primerNombre",
+                    "apellido-repartidor": "$rep_primerApellido",
+                    "telefono-repartidor": "$rep_telefono",
+                    "correo-repartidor": "$rep_email",
+                    "estado-repartidor": "$rep_estado",
+                    "genero-repartidor": "$rep_genero",
+                    "vehiculo-repartidor": "$rep_codigo_vehiculo",
+                }
+            }
+        ]).toArray();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
