30a3e1174370b15cc372c624bc97483c63578583
Braian-Quintian <bquintianromero@gmail.com>
2023-08-28 13:57:22 -0500

    feat: :sparkles: Se añade el código para hacer la busqueda por el parametro

--- src/versiones/v1/repartidores.js
+++ src/versiones/v1/repartidores.js
@@ -1,3 +1,4 @@
+import { validationResult } from 'express-validator';
 import { connect } from '../../connection/connection.js'
 const db = await connect();
 
@@ -24,3 +25,31 @@ export const repartidoresV1 = async (req,res,next) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const repartidoresV1Id = async (req, res, next) => {
+    if (!req.rateLimit) return;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    try {
+      const result = await db.collection('repartidores').findOne(
+        { rep_dni: parseInt(req.params.id) },
+        {
+          projection: {
+            _id: 0,
+            "dni-repartidor": "$rep_dni",
+            "nombre-repartidor": "$rep_primerNombre",
+            "apellido-repartidor": "$rep_primerApellido",
+            "telefono-repartidor": "$rep_telefono",
+            "correo-repartidor": "$rep_email",
+            "estado-repartidor": "$rep_estado",
+            "genero-repartidor": "$rep_genero",
+            "vehiculo-repartidor": "$rep_codigo_vehiculo",
+          },
+        }
+      );
+      if (!result) return res.status(404).json({ message: 'No se encontró el repartidor' });
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+};  
