2f1b26a20ad3cc04f86e592bd1ff3710198bea10
dVera17 <dsvera174@gmail.com>
2023-08-28 19:49:44 -0500

    feat: :sparkles: Se agrega el endpoint para restaurantes
    Se agrega el endpoint para restaurantes

--- server/dynamicRouter.js
+++ server/dynamicRouter.js
@@ -1,5 +1,6 @@
 import { router as Repartidores } from '../src/middleware/repartidores.js';
 import { router as Clientes } from '../src/routes/clientes.routes.js';
+import { router as Restaurante } from '../src/routes/restaurante.routes.js';
 export async function dynamicRouter(req, res, next) {
     const { collection } = req.params;
     switch (collection) {
@@ -7,6 +8,8 @@ export async function dynamicRouter(req, res, next) {
             return Repartidores(req, res, next);
         case 'clientes':
             return Clientes(req, res, next);
+        case 'restaurante':
+            return Restaurante(req, res, next);
         default:
             return res.status(404).send({ error: 'Ruta no encontrada' });
     }
