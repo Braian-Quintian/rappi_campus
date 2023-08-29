cdbdec702c71c6c887837864318c29a75f02f2d0
dVera17 <dsvera174@gmail.com>
2023-08-28 21:19:05 -0500

    feat: :sparkles: Endpoint para empleado
    Endpoint para empleado

---
+++ src/routes/empleados.routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import routesVersioning from 'express-routes-versioning';
+import { limitRepartidores } from "../config/limit.js";
+import { validateDTOEmpleado } from "../DTO/empleados.js";
+import { deleteEmpleado, getEmpleados, postEmpleado, putEmpleado } from "../versiones/v6/empleados.js";
+const router = Router();
+const version = routesVersioning();
+
+router.use(limitRepartidores());
+router.get('/', version({
+    "6.0.0": getEmpleados
+}))
+
+router.post('/', validateDTOEmpleado, version({
+    "6.0.0": postEmpleado
+}))
+
+router.put('/:dni', version({
+    "6.0.0": putEmpleado
+}))
+
+router.delete('/:dni', version({
+    "6.0.0": deleteEmpleado
+}))
+
+export {
+    router
+}
