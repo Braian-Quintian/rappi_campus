4f94ce25ca5a2c2f91aadc81a4eb273e7f45b36c
dVera17 <dsvera174@gmail.com>
2023-08-28 21:17:53 -0500

    feat: :sparkles: Se agrega la ruta empleados
    Se agrega la ruta empleados

--- server/dynamicRouter.js
+++ server/dynamicRouter.js
@@ -1,6 +1,7 @@
 import { router as Repartidores } from '../src/middleware/repartidores.js';
 import { router as Clientes } from '../src/routes/clientes.routes.js';
 import { router as Restaurante } from '../src/routes/restaurante.routes.js';
+import { router as Empleados } from '../src/routes/empleados.routes.js';
 export async function dynamicRouter(req, res, next) {
     const { collection } = req.params;
     switch (collection) {
@@ -10,6 +11,8 @@ export async function dynamicRouter(req, res, next) {
             return Clientes(req, res, next);
         case 'restaurante':
             return Restaurante(req, res, next);
+        case 'empleados':
+            return Empleados(req, res, next);
         default:
             return res.status(404).send({ error: 'Ruta no encontrada' });
     }
