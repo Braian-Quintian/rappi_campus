03c1e42a6979cfd8613f0b6db21a8f96c20f0606
Braian-Quintian <bquintianromero@gmail.com>
2023-08-29 13:58:42 -0500

    feat: :construction: Se añade el post

--- src/routes/repartidores.routes.js
+++ src/routes/repartidores.routes.js
@@ -1,18 +1,22 @@
 import routesVersioning from 'express-routes-versioning';
 import { Router } from 'express';
-import { limitRepartidores } from '../config/limit.js';
-import {repartidoresDtoId } from '../DTO/repartidores.dto.js';
-import { repartidoresV1, repartidoresV1Id} from '../versiones/v1/repartidores.js'
+import { limitRepartidoresGET,limitRepartidoresPOST } from '../config/limit.js';
+import {repartidoresDtoId,repartidoresDtoPost } from '../DTO/repartidores.dto.js';
+import { repartidoresV1, repartidoresV1Id, repartidoresV1_1} from '../versiones/v1/repartidores.js'
 
 const repartidoresRoutes = Router();
 const version = routesVersioning();
 
-repartidoresRoutes.get('/', limitRepartidores(), version({
+repartidoresRoutes.get('/', limitRepartidoresGET(), version({
     "1.0.0": repartidoresV1
 }));
 
-repartidoresRoutes.get('/:id', repartidoresDtoId,limitRepartidores(), version({
+repartidoresRoutes.get('/:id', repartidoresDtoId,limitRepartidoresGET(), version({
     "1.0.1": repartidoresV1Id
+}));
+
+repartidoresRoutes.post('/', repartidoresDtoPost,limitRepartidoresPOST(), version({
+    "1.0.2": repartidoresV1_1
 }));
 
 export default repartidoresRoutes;
