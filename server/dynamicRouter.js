b13f58126ec4f72f920095b9f20976f9998f81af
Braian-Quintian <bquintianromero@gmail.com>
2023-08-30 01:45:13 -0500

    refactor: :truck: Se cambia el nombre del endpoint

--- server/dynamicRouter.js
+++ server/dynamicRouter.js
@@ -9,7 +9,7 @@ export async function dynamicRouter(req, res, next) {
             return Repartidores(req, res, next);
         case 'clientes':
             return Clientes(req, res, next);
-        case 'restaurante':
+        case 'restaurantes':
             return Restaurante(req, res, next);
         case 'empleados':
             return Empleados(req, res, next);
