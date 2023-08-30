a08fc8b76cfcfb847ace6b5a259ce2a4071d865a
Braian-Quintian <bquintianromero@gmail.com>
2023-08-30 01:48:36 -0500

    feat: :sparkles: Se añaden las consultas correspondientes

--- src/versiones/v4/restaurante.js
+++ src/versiones/v4/restaurante.js
@@ -1,42 +1,109 @@
 import { connect } from "../../connection/connection.js";
 import { validationResult } from "express-validator";
+import bcrypt from 'bcrypt';
 const db = await connect();
-const restaurante = db.collection('restaurante');
 
-export const postRestaurante = async (req, res) => {
+
+export const restaurantesV1 = async (req, res) => {
+    if (!req.rateLimit) return;
     try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) return res.status(400).json(errors);
-        let result = await restaurante.insertOne(req.body);
-        res.send(result);
+        const result = await db.collection('restaurantes').aggregate([
+            {
+                $project: {
+                  _id: 0,
+                  "nit-restaurante": "$res_nit",
+                  "nombre-restaurante": "$res_nombre",
+                  "correo-restaurante": "$res_email",
+                  "direccion-restaurante": "$res_direccion",
+                  "telefono-restaurante": "$res_telefono",
+                  "categoria-restaurante": "$res_categoria",
+                  "hora-apertura": "$res_hora_apertura",
+                  "hora-cierre": "$res_hora_cierre"
+                }
+              }
+        ]).toArray();
+        res.status(200).json(result);
     } catch (error) {
-        res.send(error);
+        res.status(500).json({ message: error.message });
     }
 }
 
-export const getRestautantes = async (req, res) => {
+export const restaurantesV1Id = async (req, res) => {
+    if (!req.rateLimit) return;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+  try {
+    const result = await db.collection('restaurantes').findOne(
+        { res_nit: parseInt(req.params.nit) },
+        {
+            projection: {
+                _id: 0,
+                "nit-restaurante": "$res_nit",
+                "nombre-restaurante": "$res_nombre",
+                "correo-restaurante": "$res_email",
+                "direccion-restaurante": "$res_direccion",
+                "telefono-restaurante": "$res_telefono",
+                "categoria-restaurante": "$res_categoria",
+                "hora-apertura": "$res_hora_apertura",
+                "hora-cierre": "$res_hora_cierre"
+            },
+        }
+    );
+    if (!result) return res.status(404).json({ message: 'No se encontró el restaurante' });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+export const restaurantesV1_1 = async (req, res) => {
+    if (!req.rateLimit) return;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
     try {
-        let result = await restaurante.find({}).toArray();
-        res.send(result);
+        const cryptPass = await bcrypt.hash(req.body["contraseña-restaurante"], 10);
+        const dataSend = {
+            res_nit: parseInt(req.body["nit-restaurante"]),
+            res_nombre: req.body["nombre-restaurante"],
+            res_email: req.body["correo-restaurante"],
+            res_password: cryptPass,
+            res_direccion: req.body["direccion-restaurante"],
+            res_telefono: req.body["telefono-restaurante"],
+            res_categoria: req.body["categoria-restaurante"],
+            res_hora_apertura: req.body["hora-apertura"],
+            res_hora_cierre: req.body["hora-cierre"],
+            res_permisos: {
+                "/restaurantes": ["1.0.2"],
+                "/restaurantes/:nit": ["1.0.3"],
+            }
+        }
+        await db.collection("restaurantes").insertOne(dataSend);
+        res.status(201).json({ message: "El restaurante ha sido creado exitosamente. " });
     } catch (error) {
-        res.send(error);
+        res.status(500).json({ message: error.message });
     }
 }
 
-export const putRestautantes = async (req, res) => {
+export const restaurantesV1_11 = async (req, res) => {
+    if (!req.rateLimit) return;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
     try {
-        let result = await restaurante.updateOne({ "res_nit": parseInt(req.params.nit) }, { "$set": req.body });
-        res.send(result);
+        const restauranteId = parseInt(req.params.nit);
+        const updatedData = {
+            res_telefono: req.body["telefono-restaurante"],
+            res_hora_apertura: req.body["hora-apertura"],
+            res_hora_cierre: req.body["hora-cierre"],
+          };
+          const result = await db.collection('restaurantes').updateOne(
+            { res_nit: restauranteId },
+            { $set: updatedData }
+          );
+          if (result.modifiedCount === 0) {
+            return res.status(404).json({ message: 'No se encontró el restaurante' });
+          }
+          res.status(200).json({ message: 'El restaurante se ha actualizado exitosamente' });
     } catch (error) {
-        res.send(error);
-    }
-}
-
-export const deleteRestaurante = async (req, res) => {
-    try {
-        let result = await restaurante.deleteOne({ "res_nit": parseInt(req.params.nit) })
-        res.send(result);
-    } catch (error) {
-        res.send(error);
+        res.status(500).json({ message: error.message });
     }
 }
