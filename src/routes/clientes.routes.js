1d2f974e1484279dcabe2bc9da18295a30545818
Braian-Quintian <bquintianromero@gmail.com>
2023-08-29 23:06:40 -0500

    refactor: :hammer: Se refactoriza el código

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,25 +1,25 @@
-import { Router } from "express";
 import routesVersioning from 'express-routes-versioning';
-import { deleteCliente, getClientes, postCliente, putCliente } from "../versiones/v2/clientes.js";
-import { validateDTOcliente } from "../DTO/clientes.js";
-const router = Router();
+import { Router } from "express";
+import { limitClientesGET } from '../config/limit.js';
+import { clientesDtoId, clientesDtoPost } from "../DTO/clientes.js";
+import { clientesV1, clientesV1Id,clientesV1_1, clientesv1_11 } from "../versiones/v1/clientes.js";
+const clientesRouter = Router();
 const version = routesVersioning();
-router.get('/', version({
-    "2.0.0": getClientes
+
+clientesRouter.get('/', limitClientesGET(), version({
+    "1.0.0": clientesV1
 }))
 
-router.post('/', validateDTOcliente, version({
-    "2.0.0": postCliente
+clientesRouter.get('/:id',clientesDtoId,limitClientesGET(),version({
+    "1.0.1": clientesV1Id
 }))
 
-router.put('/:dni', version({
-    "2.0.0": putCliente
+clientesRouter.post('/', clientesDtoPost, version({
+    "1.0.2": clientesV1_1
 }))
 
-router.delete('/:dni', version({
-    "2.0.0": deleteCliente
+clientesRouter.put('/:dni',clientesDtoId, version({
+    "1.0.3": clientesv1_11
 }))
 
-export {
-    router
-}
+export default clientesRouter;
