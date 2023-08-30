a7fe2bbdc438e2bdf14cb98e207881aac05212ac
Braian-Quintian <bquintianromero@gmail.com>
2023-08-30 10:39:57 -0500

    refactor: :hammer: Se refactoriza el código

--- src/routes/empleados.routes.js
+++ src/routes/empleados.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import routesVersioning from 'express-routes-versioning';
-import {limitEmpleadosGET} from '../config/limit.js';
-import { validateDTOEmpleado,empleadosDtoId } from "../DTO/empleados.js";
-import { empleadosV1,empleadosV1Id,empleadosV1_1, putEmpleado } from "../versiones/v6/empleados.js";
+import {limitEmpleadosGET,limitEmpleadosPOST,limitEmpleadosPUT} from '../config/limit.js';
+import { empleadosDtoId,empleadosDtoPost,empleadosDtoPut } from "../DTO/empleados.js";
+import { empleadosV1,empleadosV1Id,empleadosV1_1, empleadosV1_11 } from "../versiones/v6/empleados.js";
 const empleadosRouter = Router();
 const version = routesVersioning();
 
@@ -10,16 +10,16 @@ empleadosRouter.get('/',limitEmpleadosGET(), version({
     "1.0.0": empleadosV1
 }))
 
-empleadosRouter.get('/:dni',empleadosDtoId,version({
+empleadosRouter.get('/:dni',empleadosDtoId,limitEmpleadosGET(),version({
     "1.0.1": empleadosV1Id
 }))
 
-empleadosRouter.post('/', validateDTOEmpleado, version({
+empleadosRouter.post('/', empleadosDtoPost,limitEmpleadosPOST(), version({
     "1.0.2": empleadosV1_1
 }))
 
-empleadosRouter.put('/:dni', version({
-    "1.0.3": putEmpleado
+empleadosRouter.put('/:dni',empleadosDtoPut,limitEmpleadosPUT(), version({
+    "1.0.3": empleadosV1_11
 }))
 
 export default empleadosRouter;
