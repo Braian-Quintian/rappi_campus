6c4ffd17ad8053d292d50aacae802666d0547a7a
Braian-Quintian <bquintianromero@gmail.com>
2023-08-29 23:08:35 -0500

    style: :art: Se le da formato al documento

--- src/routes/repartidores.routes.js
+++ src/routes/repartidores.routes.js
@@ -1,9 +1,8 @@
 import routesVersioning from 'express-routes-versioning';
 import { Router } from 'express';
-import { limitRepartidoresGET,limitRepartidoresPOST, limitRepartidoresPUT } from '../config/limit.js';
-import {repartidoresDtoId,repartidoresDtoPost, repartidoresDtoPut } from '../DTO/repartidores.js';
-import { repartidoresV1, repartidoresV1Id, repartidoresV1_1, repartidoresV1_11} from '../versiones/v1/repartidores.js'
-
+import { limitRepartidoresGET, limitRepartidoresPOST, limitRepartidoresPUT } from '../config/limit.js';
+import { repartidoresDtoId, repartidoresDtoPost, repartidoresDtoPut } from '../DTO/repartidores.js';
+import { repartidoresV1, repartidoresV1Id, repartidoresV1_1, repartidoresV1_11 } from '../versiones/v1/repartidores.js'
 const repartidoresRoutes = Router();
 const version = routesVersioning();
 
@@ -11,15 +10,15 @@ repartidoresRoutes.get('/', limitRepartidoresGET(), version({
     "1.0.0": repartidoresV1
 }));
 
-repartidoresRoutes.get('/:id', repartidoresDtoId,limitRepartidoresGET(), version({
+repartidoresRoutes.get('/:id', repartidoresDtoId, limitRepartidoresGET(), version({
     "1.0.1": repartidoresV1Id
 }));
 
-repartidoresRoutes.post('/', repartidoresDtoPost,limitRepartidoresPOST(), version({
+repartidoresRoutes.post('/', repartidoresDtoPost, limitRepartidoresPOST(), version({
     "1.0.2": repartidoresV1_1
 }));
 
-repartidoresRoutes.put('/:id', repartidoresDtoPut,limitRepartidoresPUT(), version({
+repartidoresRoutes.put('/:id', repartidoresDtoPut, limitRepartidoresPUT(), version({
     "1.0.3": repartidoresV1_11
 }));
 
