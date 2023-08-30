da14d39fc2746a0fb60ced9d9f7829e224407324
Braian-Quintian <bquintianromero@gmail.com>
2023-08-30 01:47:49 -0500

    feat: :sparkles: Se añade dto a los enpoints

--- src/DTO/restaurante.js
+++ src/DTO/restaurante.js
@@ -1,9 +1,37 @@
-import { check } from "express-validator";
+import { param,body } from "express-validator";
 
-export const validateDTORestaurante = [
-    check('res_nit').notEmpty().isNumeric().not().isString().withMessage('res_nit es requerido y debe ser un número entero'),
-    check('res_nombre').notEmpty().isString().withMessage('res_nombre es requerido y debe ser un string'),
-    check('res_direccion').notEmpty().isString().withMessage('res_direccion es requerido y debe ser un string'),
-    check('res_telefono').notEmpty().isString().matches("^[0-9]+$").withMessage('res_telefono es requerido y solo debe contener números'),
-    check('res_categoria').notEmpty().isString().withMessage('res_categoria es requerido y debe ser un string'),
+export const restaurantesDtoId = [
+    param('nit').notEmpty().isNumeric().withMessage('El nit es requerido')
+    .matches(/^[a-zA-Z0-9 ]+$/).withMessage('El nit debe ser un numero')
+]
+
+export const restaurantesDtoPost = [
+    body('nit-restaurante').notEmpty().isNumeric().not().isString().withMessage('El nit es requerido')
+    .matches(/^[a-zA-Z0-9 ]+$/).withMessage('El nit debe ser un numero'),
+    body('nombre-restaurante').notEmpty().isString().withMessage('El nombre del restaurante es requerido')
+    .matches(/^[a-zA-Z0-9 ]+$/).withMessage('El nombre debe ser un string'),
+    body('correo-restaurante').notEmpty().isEmail().withMessage('El correo debe ser un email y es requerido'),
+    body('contraseña-restaurante').notEmpty().isString().withMessage('La contraseña es requerida')
+    .matches(/^[a-zA-Z0-9]+$/).withMessage('La contraseña debe ser un string'),
+    body('direccion-restaurante').notEmpty().isString().withMessage('La dirección es requerida')
+    .matches(/^[a-zA-Z0-9 ]+$/).withMessage('La dirección debe ser un string'),
+    body('telefono-restaurante').notEmpty().isNumeric().withMessage('El telefono es requerido')
+    .matches(/^[0-9]+$/).withMessage('El telefono debe ser un numero'),
+    body('categoria-restaurante').notEmpty().isString().withMessage('La categoria es requerida')
+    .matches(/^[a-zA-Z0-9 ]+$/).withMessage('La categoria debe ser un string'),
+    body('hora-apertura').notEmpty().withMessage('La hora de apertura es requerida')
+    .matches(/^\d{2}:\d{2}$/).withMessage('La hora de apertura debe tener el formato HH:MM'),
+    body('hora-cierre').notEmpty().withMessage('La hora de cierre es requerida')
+    .matches(/^\d{2}:\d{2}$/).withMessage('La hora de cierre debe tener el formato HH:MM'),
+]
+
+export const restaurantesDtoPut = [
+    param('nit').notEmpty().isNumeric().withMessage('El nit es requerido')
+    .matches(/^[a-zA-Z0-9 ]+$/).withMessage('El nit debe ser un numero'),
+    body('telefono-restaurante').notEmpty().isNumeric().withMessage('El telefono es requerido')
+    .matches(/^[0-9]+$/).withMessage('El telefono debe ser un numero'),
+    body('hora-apertura').notEmpty().withMessage('La hora de apertura es requerida')
+    .matches(/^\d{2}:\d{2}$/).withMessage('La hora de apertura debe tener el formato HH:MM'),
+    body('hora-cierre').notEmpty().withMessage('La hora de cierre es requerida')
+    .matches(/^\d{2}:\d{2}$/).withMessage('La hora de cierre debe tener el formato HH:MM'),
 ]
