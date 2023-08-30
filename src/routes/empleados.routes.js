bb187d8bec97d04daea32dff03c6037b79300058
Braian-Quintian <bquintianromero@gmail.com>
2023-08-30 02:04:05 -0500

    refactor: :construction: Se está refactorizando el código

--- src/routes/empleados.routes.js
+++ src/routes/empleados.routes.js
@@ -1,23 +1,25 @@
 import { Router } from "express";
 import routesVersioning from 'express-routes-versioning';
-import { validateDTOEmpleado } from "../DTO/empleados.js";
-import { deleteEmpleado, getEmpleados, postEmpleado, putEmpleado } from "../versiones/v6/empleados.js";
+import {limitEmpleadosGET} from '../config/limit.js';
+import { validateDTOEmpleado,empleadosDtoId } from "../DTO/empleados.js";
+import { empleadosV1,empleadosV1Id,empleadosV1_1, putEmpleado } from "../versiones/v6/empleados.js";
 const empleadosRouter = Router();
 const version = routesVersioning();
-empleadosRouter.get('/', version({
-    "6.0.0": getEmpleados
+
+empleadosRouter.get('/',limitEmpleadosGET(), version({
+    "1.0.0": empleadosV1
+}))
+
+empleadosRouter.get('/:dni',empleadosDtoId,version({
+    "1.0.1": empleadosV1Id
 }))
 
 empleadosRouter.post('/', validateDTOEmpleado, version({
-    "6.0.0": postEmpleado
+    "1.0.2": empleadosV1_1
 }))
 
 empleadosRouter.put('/:dni', version({
-    "6.0.0": putEmpleado
-}))
-
-empleadosRouter.delete('/:dni', version({
-    "6.0.0": deleteEmpleado
+    "1.0.3": putEmpleado
 }))
 
 export default empleadosRouter;
