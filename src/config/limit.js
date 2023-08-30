be25d0905e731b7cf7283e91d66b3cf97da8470c
Braian-Quintian <bquintianromero@gmail.com>
2023-08-29 22:43:58 -0500

    feat: :sparkles: Se añade el limit para get clientes

--- src/config/limit.js
+++ src/config/limit.js
@@ -52,5 +52,16 @@ export const limitRepartidoresPUT = ()=> {
         }, 
         message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
         }
-    })      
+    })
+}
+
+export const limitClientesGET = ()=> {
+    return rateLimit({
+        windowMs: 30 * 1000,
+        max: 5,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
+        }
+    })  
 }
