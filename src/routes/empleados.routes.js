862ae3897c98c07f1941991a6e0b31b3e8b80817
Braian-Quintian <bquintianromero@gmail.com>
2023-08-29 23:07:06 -0500

    refactor: :hammer: Se refactoriza el código

--- src/routes/empleados.routes.js
+++ src/routes/empleados.routes.js
@@ -2,24 +2,22 @@ import { Router } from "express";
 import routesVersioning from 'express-routes-versioning';
 import { validateDTOEmpleado } from "../DTO/empleados.js";
 import { deleteEmpleado, getEmpleados, postEmpleado, putEmpleado } from "../versiones/v6/empleados.js";
-const router = Router();
+const empleadosRouter = Router();
 const version = routesVersioning();
-router.get('/', version({
+empleadosRouter.get('/', version({
     "6.0.0": getEmpleados
 }))
 
-router.post('/', validateDTOEmpleado, version({
+empleadosRouter.post('/', validateDTOEmpleado, version({
     "6.0.0": postEmpleado
 }))
 
-router.put('/:dni', version({
+empleadosRouter.put('/:dni', version({
     "6.0.0": putEmpleado
 }))
 
-router.delete('/:dni', version({
+empleadosRouter.delete('/:dni', version({
     "6.0.0": deleteEmpleado
 }))
 
-export {
-    router
-}
+export default empleadosRouter;
