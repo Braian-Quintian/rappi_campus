894b107ee0e17c34b1af620d93a985305af307d4
Braian-Quintian <bquintianromero@gmail.com>
2023-08-29 21:55:40 -0500

    refactor: :hammer: Se cambian la forma en la que se hacen las solicitudes GET

--- src/versiones/v2/clientes.js
+++ src/versiones/v2/clientes.js
@@ -1,14 +1,54 @@
-import { connect } from "../../connection/connection.js";
 import { validationResult } from "express-validator";
+import { connect } from "../../connection/connection.js";
 const db = await connect();
-const clientes = db.collection('clientes');
 
-export const getClientes = async (req, res) => {
+export const clientesV1 = async (req, res) => {
+    if (!req.rateLimit) return;
     try {
-        let result = await clientes.find({}).toArray();
-        res.send(result);
+        const result = await db.collection('clientes').aggregate([
+            {
+                $project: {
+                    _id: 0,
+                    "cliente-dni": "$cli_dni",
+                    "cliente-nombre": "$cli_primerNombre",
+                    "cliente-apellido": "$cli_primerApellido",
+                    "cliente-telefono": "$cli_telefono",
+                    "cliente-email": "$cli_email",
+                    "cliente-fecha-nacimiento": "$cli_fecha_nac",
+                    "cliente-genero": "$cli_genero"
+                }
+            }
+        ]).toArray();
+        res.status(200).json(result);
     } catch (error) {
-        res.send(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const clientesV1Id = async (req, res) => {
+    if (!req.rateLimit) return;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    try {
+        const result = await db.collection('clientes').findOne(
+            { rep_dni: parseInt(req.params.id) },
+            {
+                projection: {
+                    _id: 0,
+                    "cliente-dni": "$cli_dni",
+                    "cliente-nombre": "$cli_primerNombre",
+                    "cliente-apellido": "$cli_primerApellido",
+                    "cliente-telefono": "$cli_telefono",
+                    "cliente-email": "$cli_email",
+                    "cliente-fecha-nacimiento": "$cli_fecha_nac",
+                    "cliente-genero": "$cli_genero"
+                },
+            }
+        );
+        if (!result) return res.status(404).json({ message: 'No se encontró el repartidor' });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 }
 
@@ -46,15 +86,6 @@ export const putCliente = async (req, res) => {
         };
         let result = await clientes.updateOne({ cli_dni: parseInt(req.params.dni) }, { "$set": clienteData });
         console.log(result);
-        res.send(result);
-    } catch (error) {
-        res.send(error);
-    }
-}
-
-export const deleteCliente = async (req, res) => {
-    try {
-        let result = await clientes.deleteOne({ "cli_dni": parseInt(req.params.dni) })
         res.send(result);
     } catch (error) {
         res.send(error);
