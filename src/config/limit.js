80576e4add38ee499180794ded8d2cfbf318d4ff
Braian-Quintian <bquintianromero@gmail.com>
2023-09-04 13:07:48 -0500

    feat: :sparkles: Se añade el limit para pedidos y para detalles

--- src/config/limit.js
+++ src/config/limit.js
@@ -218,3 +218,85 @@ export const limitProductosPUT = ()=> {
         }
     })
 }
+
+export const limitPedidosGET = ()=> {
+    return rateLimit({
+        windowMs: 30 * 1000,
+        max: 10,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
+        }
+    })
+}
+
+export const limitPedidosPOST = ()=> {
+    return rateLimit({
+        windowMs: 60 * 1000,
+        max: 10,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        skip: (req,res)=>{if(req.headers["content-length"]>1000){res.status(413).send({message: "Tamaño de la solicitud alcanzado"});
+                return true;
+            }
+        }, 
+        message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
+        }
+    })
+}
+
+export const limitPedidosPUT = ()=> {
+    return rateLimit({
+        windowMs: 60 * 1000,
+        max: 10,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        skip: (req,res)=>{if(req.headers["content-length"]>1000){res.status(413).send({message: "Tamaño de la solicitud alcanzado"});
+                return true;
+            }
+        }, 
+        message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
+        }
+    })
+}
+
+export const limitDetallesGET = ()=> {
+    return rateLimit({
+        windowMs: 30 * 1000,
+        max: 10,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
+        }
+    })
+}
+
+export const limitDetallesPOST = ()=> {
+    return rateLimit({
+        windowMs: 60 * 1000,
+        max: 10,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        skip: (req,res)=>{if(req.headers["content-length"]>1000){res.status(413).send({message: "Tamaño de la solicitud alcanzado"});
+                return true;
+            }
+        }, 
+        message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
+        }
+    })
+}
+
+export const limitDetallesPUT = ()=> {
+    return rateLimit({
+        windowMs: 60 * 1000,
+        max: 10,
+        standardHeaders: true, 
+        legacyHeaders: false, 
+        skip: (req,res)=>{if(req.headers["content-length"]>1000){res.status(413).send({message: "Tamaño de la solicitud alcanzado"});
+                return true;
+            }
+        }, 
+        message: (req,res)=>{res.status(429).send({message: "Limite alcanzado"});
+        }
+    })
+}
